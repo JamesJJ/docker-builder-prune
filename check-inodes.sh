c0942fd597ad4de562ccf1222668d8154f7d2398
JamesJJ <jj@fcg.fyi>
2019-11-09 19:48:39 +0800

    Update identifier string

--- check-inodes.sh
+++ check-inodes.sh
@@ -5,6 +5,11 @@ THRESHOLD="${THRESHOLD:-50}"
 FILTER="${FILTER:-unused-for=1h}"
 SLEEP="${SLEEP:-600}"
 
+IDENTIFIER="${IDENTIFIER:-$(hostname -s)}"
+if [ ! -z "${HOST_IP}" ]; then
+  IDENTIFIER="${IDENTIFIER}:${HOST_IP}"
+fi
+
 PATH=$PATH:/usr/local/bin
 
 sleep 60
@@ -15,11 +20,11 @@ while true; do
     if [ ! -d "${MP}" ]; then continue; fi
     PERCENT="$(df --output=ipcent "${MP}" | tail -n 1 | sed 's|[^[:digit:]]*||g')"
     if [ "${PERCENT}" -lt "${THRESHOLD}" ]; then
-      printf '[%s] inode usage %d%% for %s - No action taken\n' "$(hostname -s)" "${PERCENT}" "${MP}"
+      printf '[%s] inode usage %d%% for %s - No action taken\n' "${IDENTIFIER}" "${PERCENT}" "${MP}"
     else
-      printf '[%s] inode usage %d%% for %s - Pruning\n' "$(hostname -s)" "${PERCENT}" "${MP}"
+      printf '[%s] inode usage %d%% for %s - Pruning\n' "${IDENTIFIER}" "${PERCENT}" "${MP}"
       docker builder prune --force --filter "${FILTER}"
-      printf '[%s] prune finished\n' "$(hostname -s)"
+      printf '[%s] prune finished\n' "${IDENTIFIER}"
       sleep 900
     fi
   done
